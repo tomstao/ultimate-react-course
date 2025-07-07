19ba40201c2e4443664bdc8edc4cd8abea9f3e55
Tao Su <tao@agentichealth.ai>
2025-07-07 14:52:14 -0400

    conditional rendering

--- 03-pizza-menu/starter/pizza-menu/src/index.js
+++ 03-pizza-menu/starter/pizza-menu/src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 const pizzaData = [
     {
         name: "Focaccia",
-        ingredients: "Bread with italian olive oil and rosemary",
+        ingredients: "Bread with italian olive oil and rosemary.",
         price: 6,
         photoName: "pizzas/focaccia.jpg",
         soldOut: false,
